b6049d6a0c89859f24ef82ec3c068ed0de66dfb5
Joenathan haganta ginting <jojobrelingga@gmail.com>
2025-01-27 21:41:16 +0700

    Menghapus fitur Portofolio 1

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -5,22 +5,13 @@ export default {
       activeTab: 1,
       items: [
         {
-          id: 1,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
-          status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
-          tech: 'Laravel 8, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
           id: 2,
           name: 'Sahabat Muslim',
           imageUrl: 'sahabatmuslim',
           status: 'A website that has a responsive display to make it easier for Muslims to read the Quran from anywhere.',
           tech: 'VueJS 3, Tailwind',
           github: 'null',
-          demo: 'https://sahabat-muslim.netlify.app/'
+          demo: 'null'
         },
         {
           id: 3,
@@ -28,15 +19,6 @@ export default {
           imageUrl: 'sistekdes',
           status: 'Sistem Informasi dan Teknologi Desa, a web application to assist village government work in administration, population data management, community services, etc.',
           tech: 'Laravel 9, Bootstrap',
-          github: 'null',
-          demo: 'null'
-        },
-        {
-          id: 4,
-          name: 'Bumdesma',
-          imageUrl: 'bumdesma',
-          status: 'a web application used by village-owned enterprises at the sub-district level to make it easier for them in terms of accounting that is equipped with a microfinance balance.',
-          tech: 'Laravel 8, Bootstrap',
           github: 'null',
           demo: 'null'
         },
