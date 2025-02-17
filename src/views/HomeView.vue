414ff5e5d8408efa855c02477b907fa4d4b7f3e8
NathanzZ-Ginting <jojobrelingga@gmail.com>
2025-02-17 14:38:57 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <main class="mt-10 md:mt-1 flex flex-col-reverse gap-8 items-center md:flex-row md:gap-16 md:justify-center min-h-[65vh] md:min-h-[80vh]">
     <div ref="textContainer" class="space-y-2 text-center md:text-left px-10 opacity-0">
       <p class="text-amber-200">Hello World, I'm</p>
-      <h1 ref="name" class="text-4xl font-bold md:text-5xl text-white">Joenathan Haganta Ginting</h1>
+      <h1 ref="name" class="text-4xl font-bold md:text-5xl text-white">MARDY PEPEK KONTOL</h1>
       <div class="py-2">
         <h1 ref="title" class="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-yellow-500 md:text-2xl">
           Fullstack Developer & Software Engineer
